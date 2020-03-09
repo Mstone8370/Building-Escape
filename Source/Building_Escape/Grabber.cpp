5388d67d9b594f7f4e44c9adc453962da3c0a8af
Mstone8370 <mstone8370@gmail.com>
2020-03-09 14:36:49 +0900

    Getting Player Viewpoint

--- Source/Building_Escape/Grabber.cpp
+++ Source/Building_Escape/Grabber.cpp
@@ -1,6 +1,7 @@
 // Copyright test.
 
-
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 #include "Grabber.h"
 
 // Sets default values for this component's properties
@@ -30,5 +31,18 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotator;
+
+	this->GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		PlayerViewPointLocation,
+		PlayerViewPointRotator
+		);
+
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), 
+		*PlayerViewPointLocation.ToString(),
+		*PlayerViewPointRotator.ToString()
+		);
+
 }
 
