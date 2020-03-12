0a3f26cf82c9e431a5e6ac3c5af6a45d13df6c0d
Mstone8370 <mstone8370@gmail.com>
2020-03-12 13:58:27 +0900

    123. Reducing Code in 'Hot Loops'

--- Source/Building_Escape/Grabber.cpp
+++ Source/Building_Escape/Grabber.cpp
@@ -11,43 +11,47 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	// UE_LOG(LogTemp, Warning, TEXT("test"));
+	FindPhysicsHandle();
+	SetupInputComponent();
+}
+
+void UGrabber::FindPhysicsHandle() {
 	PhysicsHandle = this->GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
-	if(PhysicsHandle != nullptr) {
-		UE_LOG(LogTemp, Warning, TEXT("PhysicsHandle found"));
-	} else {
+	if(PhysicsHandle == nullptr) {
 		UE_LOG(LogTemp, Error, TEXT("No PhysicsHandle found on %s"), *GetOwner()->GetName());
+		return;
 	}
 
+	UE_LOG(LogTemp, Warning, TEXT("PhysicsHandle found"));
+}
+
+void UGrabber::SetupInputComponent() {
 	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
-	if(InputComponent != nullptr) {
-		UE_LOG(LogTemp, Warning, TEXT("Input Component found"));
-		InputComponent->BindAction(TEXT("Grab"), IE_Pressed, this, &UGrabber::Grab);
-		InputComponent->BindAction(TEXT("Grab"), IE_Released, this, &UGrabber::Release);
-	} else {
+	if(InputComponent == nullptr) {
 		UE_LOG(LogTemp, Error, TEXT("No Input Component found on %s"), *GetOwner()->GetName());
+		return;
 	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("Input Component found"));
+	InputComponent->BindAction(TEXT("Grab"), IE_Pressed, this, &UGrabber::Grab);
+	InputComponent->BindAction(TEXT("Grab"), IE_Released, this, &UGrabber::Release);
 }
 
 void UGrabber::Grab() {
 	UE_LOG(LogTemp, Warning, TEXT("Grabber Pressed"));
+	GetFirstPhysicsBodyInReach();
 }
 
 void UGrabber::Release() {
 	UE_LOG(LogTemp, Warning, TEXT("Grabber Released"));
 }
-
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -55,6 +59,10 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+	
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const {
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 
@@ -63,23 +71,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		PlayerViewPointRotation
 	);
 
-	// UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), 
-	// 	*PlayerViewPointLocation.ToString(),
-	// 	*PlayerViewPointRotation.ToString()
-	// );
-
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
-
-	DrawDebugLine(
-		this->GetWorld(),
-		PlayerViewPointLocation,
-		LineTraceEnd,
-		FColor(0, 255, 0),
-		false,
-		0.f,
-		0,
-		5.f
-	);
 
 	FHitResult Hit;
 	FCollisionQueryParams TraceParams(FName(TEXT("")), false, this->GetOwner());
@@ -92,16 +84,13 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		TraceParams
 	);
 
+	// Debug
 	AActor* ActorHit = Hit.GetActor();
 
-	// if(ActorHit != nullptr) {
-	// 	UE_LOG(LogTemp, Warning, TEXT("Line trace has hit: %s"), *ActorHit->GetName());
-	// } else {
-	// 	UE_LOG(LogTemp, Warning, TEXT("Line trace has hit: Nothing"));
-	// }
 	if(ActorHit != nullptr) {
 		UE_LOG(LogTemp, Warning, TEXT("Line trace has hit: %s"), *ActorHit->GetName());
 	}
+	// end
 
+	return Hit;
 }
-
